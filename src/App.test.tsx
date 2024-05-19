199f44be0337333be7161d731f49c2a8316094d5
kevlarmonk <kcar@kevlarai.com>
2024-05-19 20:08:50 +0100

    Added HashRouter from react-router-dom: - dummy routes added - unit tests for not found page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
-import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { WrappedApp, App } from './App';
 
 describe('App', () => {
     it('Renders hello world', () => {
         //  ARRANGE
-        render(<App />);
+        render(<WrappedApp />);
         //  ACT
         //  EXPECT
         expect(
@@ -14,5 +14,20 @@ describe('App', () => {
                 level: 1,
             }
         )).toHaveTextContent('Hello World')
+    });
+    it('Renders not found if invalid path', () => {
+        //  ARRANGE
+        render(
+            <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+                <App />
+            </MemoryRouter>
+        );
+        //  ACT
+        //  EXPECT
+        expect(
+            screen.getByRole('heading', {
+                level: 1,
+            }
+        )).toHaveTextContent('Not Found')
     });
 });
